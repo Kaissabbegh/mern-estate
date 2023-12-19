ad0a598f9e3e21e29e94ed75d9a71ad755edf46c
Younes Sabbegh <kaissabbegh@gmail.com>
2023-12-19 16:18:58 +0100

    complete signUP page func

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use("/api/auth",authRouter);
 app.use ((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message =err.message || 'internal Server Error'
-    return res.status (statusCode).json({
+    return res.status(statusCode).json({
         success: false,
         statusCode,
         message
